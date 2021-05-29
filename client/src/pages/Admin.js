48e6508dd0f160ff61d59991f70f10f5db04c706
Joy Lindstrom <joy.lindstrom3@gmail.com>
2021-05-28 21:35:51 -0600

    admin table shows name

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar"
-const Admin = () => {
+import React, { useEffect, useState } from 'react';
+
+/*const Admin = () => {
     return (
         <div >
             <Navbar pageName="Admin" />
@@ -9,6 +11,54 @@ const Admin = () => {
             </div>
         </div>
     );
+}*/
+
+const ClientList = () => {
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        const getClients = async () => {
+            let response = await fetch('/client');
+            let data = await response.json();
+            setRows(data);
+        };
+        getClients();
+
+    }, []);
+
+    return (
+        
+     
+
+        <div className="all-clients">
+            <div >
+            <Navbar pageName="Admin" />
+
+            <div >
+                <h1>Admin</h1>
+            </div>
+        </div>
+            <table>
+                
+                <tr><th>First Name</th><th>Last Name</th></tr>
+                {rows.map((row) => {
+                    return (                                     
+                        <tr key={row.name} >
+                            <td>{row.firstName}</td>
+                            <td>{row.lastName}</td>
+                          
+                        
+                        </tr>
+                    )
+                })}                
+               
+            </table>
+        </div>
+
+    );
 }
 
-export default Admin;
+
+export default ClientList;
+
+
